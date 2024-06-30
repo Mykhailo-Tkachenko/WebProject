8d4d05c04ecbc8ae0f76c0b28a9c69e522b5e8af
Mykhailo-Tkachenko <150828483+Mykhailo-Tkachenko@users.noreply.github.com>
2024-06-30 16:07:07 +0300

    fix1 build step

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Layout } from './Components/Layout/layout'
-import { Counter } from './Components/Counter/counter'
-import userClient from './clients/user'
-
-userClient.init(
-  'https://mapstorage-7e78.restdb.io',
-  // eslint-disable-next-line no-undef
-  process.env.API_KEY,
-  fetch
-)
+// import { Layout } from './Components/Layout/layout'
+// import { Counter } from './Components/Counter/counter'
+// import userClient from './clients/user'
+//
+// userClient.init(
+//   'https://mapstorage-7e78.restdb.io',
+//   // eslint-disable-next-line no-undef
+//   process.env.API_KEY,
+//   fetch
+// )
 
 const App = () => {
   return (
     <div>
-      <Layout>
-        <p>Hello from React !!!!</p>
-        <div>Test</div>
-        <Counter active={true} type="done" value={12} />
-      </Layout>
+      <p>Hello from React !!!!</p>
+      <div>Test</div>
+      {/*<Counter active={true} type="done" value={12} />*/}
     </div>
   )
 }
